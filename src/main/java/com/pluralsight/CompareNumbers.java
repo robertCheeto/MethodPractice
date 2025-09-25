8df28f0dc9c4391918ec07077f350a909db1b0b8
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-09-25 11:52:07 -0400

    * created new CompareNumbers.java file in com.pluralsight package * added isEven() and isPositive() main with if and else statements to determine if number is even AND if number is positive

---
+++ src/main/java/com/pluralsight/CompareNumbers.java
@@ -0,0 +1,26 @@
+package com.pluralsight;
+
+public class CompareNumbers {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isEven(int number) {
+        if (number % 2 == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public static boolean isPositive(double number) {
+        if (number > 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+}
